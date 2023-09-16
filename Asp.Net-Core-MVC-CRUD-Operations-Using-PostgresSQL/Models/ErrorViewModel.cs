1ea848bb2a40075cd88e127f1b103dbb9ac6168c
JessonWork <jessonpascua.work@gmail.com>
2023-09-16 18:28:43 +0800

    Add project files.

---
+++ Asp.Net-Core-MVC-CRUD-Operations-Using-PostgresSQL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.Net_Core_MVC_CRUD_Operations_Using_PostgresSQL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
